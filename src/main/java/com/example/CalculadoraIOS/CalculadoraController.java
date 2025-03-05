3c15a629608722334118e13f6021a34e15f43989
Cassiane Cristine <cassiane_cristine@hotmail.com>
2025-03-04 22:45:07 -0300

    Correção de erros, adição de novas funcionalidades Controller, detalhes em cor.

--- src/main/java/com/example/CalculadoraIOS/CalculadoraController.java
+++ src/main/java/com/example/CalculadoraIOS/CalculadoraController.java
@@ -1,14 +1,64 @@
 package com.example.CalculadoraIOS;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
+
 public class CalculadoraController {
-    @FXML
-    private Label welcomeText;
+
+    private long num1;
+    private String operador;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label output;
+
+
+    @FXML
+    private void processTeclado(ActionEvent event){
+        String value = ((Button)event.getSource()).getText();
+        output.setText(output.getText() + value);
     }
+
+    @FXML
+    private void processOperador(ActionEvent event){
+        String value = ((Button)event.getSource()).getText();
+        if (!value.equals("=")) {
+            if (!operador.isEmpty()){
+                return;
+            }
+            operador = value;
+            num1 = Long.parseLong(output.getText());
+            output.setText("");
+        } else {
+            if(operador.isEmpty()){
+                return;
+            }
+            if(output.getText().isEmpty()){
+                output.setText("ERRO");
+                operador = "";
+            }
+            output.setText(calcular(num1,Long.parseLong(output.getText()),operador));
+            operador = "";
+        }
+    }
+
+    private String calcular(long num1, long num2, String op){
+        switch(op){
+            case"+":
+                return String.valueOf(num1 + num2);
+            case "-":
+                return String.valueOf(num1 - num2);
+            case "x":
+                return String.valueOf(num1 * num2);
+            case"÷":
+                if (num2 == 0){
+                    return "ERRO";
+                }
+                return String.valueOf(num1 / num2);
+        }
+        return "ERRO";
+    }
+
 }
--- src/main/java/com/example/CalculadoraIOS/CalculadoraIOS.java
+++ src/main/java/com/example/CalculadoraIOS/CalculadoraIOS.java
@@ -2,19 +2,23 @@ package com.example.CalculadoraIOS;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-import java.io.IOException;
 
 public class CalculadoraIOS extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(CalculadoraIOS.class.getResource("calculadora-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 298, 537);
-        stage.setTitle("CalculadoraIOS");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("calculadora-view.fxml"));
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.setTitle("CalculadoraIOS");
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
